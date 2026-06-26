34d97833f4224030781c39c9a845936ae795e2bc
GraySS <doublebump123@gmail.com>
2026-06-26 16:50:56 +0200

    feat(compiler/syntax): updated `VariableDeclarationStatement.java` to accept `variableDeclarator` directly.

--- modules/compiler/core/src/main/java/com/orca/compiler/core/syntax/statements/VariableDeclarationStatement.java
+++ modules/compiler/core/src/main/java/com/orca/compiler/core/syntax/statements/VariableDeclarationStatement.java
@@ -1,19 +1,15 @@
 package com.orca.compiler.core.syntax.statements;
 
-import com.orca.compiler.core.syntax.ExpressionSyntax;
 import com.orca.compiler.core.syntax.StatementSyntax;
-import com.orca.compiler.core.syntax.SyntaxToken;
 import com.orca.compiler.core.syntax.SyntaxVisitor;
-import com.orca.compiler.core.syntax.nodes.SimpleIdentifierSyntax;
 import com.orca.compiler.core.syntax.nodes.VariableDeclaratorSyntax;
-import com.orca.compiler.core.syntax.types.TypeSyntax;
 
 public class VariableDeclarationStatement extends StatementSyntax {
 
     private final VariableDeclaratorSyntax variableDeclarator;
 
-    public VariableDeclarationStatement(SyntaxToken modifierToken, SimpleIdentifierSyntax identifier, TypeSyntax type, ExpressionSyntax initializer) {
-        this.variableDeclarator = new VariableDeclaratorSyntax(modifierToken, identifier, type, initializer);
+    public VariableDeclarationStatement(VariableDeclaratorSyntax variableDeclarator) {
+        this.variableDeclarator = variableDeclarator;
     }
 
     /**
